8ae94ab1ad3f8bc70ffcfba4781727576f72aa9d
Daniya Niazi <farihadania@hotmail.com>
2021-10-09 19:05:43 +0500

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
-	apiKey: 'AIzaSyAs9WYcIxuGISQbYNjlV-NO5lo5D-iWeKY',
-	authDomain: 'vue-chat-app-a0d64.firebaseapp.com',
-	projectId: 'vue-chat-app-a0d64',
-	storageBucket: 'vue-chat-app-a0d64.appspot.com',
-	messagingSenderId: '768798708741',
-	appId: '1:768798708741:web:49dd6372e593a14f0a786c',
-	measurementId: 'G-H63CC27SFG',
+	
 };
 
 const app = firebase.initializeApp(firebaseConfig);
